74d85b1f92d5d9c6bede8a3e30d62315ec190769
Broc Geronimi <bg17roxs@hotmail.com>
2025-02-26 11:10:36 +1100

    integrated backend with frontend. Frontend however is currently bugged.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import React from 'react';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
